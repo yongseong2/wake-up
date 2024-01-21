ca46b1f2e4296a45f81dc0fb52f1368b4e8ee0d3
yongseong2 <ancjs369@naver.com>
2024-01-21 20:31:41 +0900

    Init: Next js 프로젝트 설정

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
+
+// Configuration object tells the next-pwa plugin
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
