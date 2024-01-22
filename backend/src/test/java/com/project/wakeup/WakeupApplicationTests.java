a45e14c85a694496831dc9adc09ac38002921c50
DJ-archive <ydajeong7@gmail.com>
2024-01-22 23:41:49 +0900

    Init Backend Project

---
+++ backend/src/test/java/com/project/wakeup/WakeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wakeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
