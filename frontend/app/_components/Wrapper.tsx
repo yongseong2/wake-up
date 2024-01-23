74af9deef2bacdb6fb0e500e82018a07deaa5cc5
yongseong2 <ancjs369@naver.com>
2024-01-23 22:18:15 +0900

    Feat: 아이콘 버튼 컴포넌트 구현 및 Wrapper 컴포넌트 수정

--- frontend/app/_components/Wrapper.tsx
+++ frontend/app/_components/Wrapper.tsx
@@ -2,11 +2,15 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  style?: string;
 }
 
-export function Wrapper({ children }: Props) {
+export function Wrapper({
+  style = "justify-between py-20 px-10",
+  children,
+}: Props) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between py-20 px-10">
+    <div className={`flex min-h-screen flex-col items-center ${style}`}>
       {children}
     </div>
   );
