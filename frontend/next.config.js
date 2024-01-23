a167d5257416e5ab6373fe4bff0f4116e111ed4d
yongseong2 <ancjs369@naver.com>
2024-01-23 22:16:55 +0900

    Feat: svg아이콘을 위한 라이브러리 설치 및 아이콘 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,32 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
+  webpack(config, options) {
+    // SVGR과 관련된 설정 추가
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
-// Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
